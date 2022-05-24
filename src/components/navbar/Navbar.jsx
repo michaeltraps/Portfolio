6ca58590a0b103d58dfce2907f1b27b9814d1a0a
Michael <mtrapani27@gmail.com>
2022-05-24 09:25:21 -0400

    started gradient work

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
+    <div className='trapani__navbar'>
+      <div className='trapani__navbar-links'>
+        <div className='trapani__navbar-links_logo'>
+          {/* <img src={logo} alt='logo' /> */}
+        </div>
+      </div>
     </div>
   );
 };
