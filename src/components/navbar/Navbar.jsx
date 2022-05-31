9b59c0e9246b35f9df564d57c3d9e0da0e504e3b
Michael <mtrapani27@gmail.com>
2022-05-31 17:57:03 -0400

    navbar updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.png';
+
+const Menu = () => (
+  <>
+    <p>
+      <a href='#welcome'>Welcome</a>
+    </p>
+    <p>
+      <a href='#projects'>Projects</a>
+    </p>
+    <p>
+      <a href='#skills'>Skills</a>
+    </p>
+    <p>
+      <a href='#resume'>Resume</a>
+    </p>
+  </>
+);
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='trapani__navbar'>
       <div className='trapani__navbar-links'>
         <div className='trapani__navbar-links_logo'>
-          {/* <img src={logo} alt='logo' /> */}
+          <img src={logo} alt='logo' />
         </div>
+        <div className='trapani__navbar-links_container'>
+          <Menu />
+        </div>
+      </div>
+      <div className='trapani__navbar-menu'>
+        {toggleMenu ? (
+          <RiCloseLine
+            color='#fff'
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color='#fff'
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className='trapani__navbar-menu_container swing-in-top-fwd'>
+            <div className='trapani__navbar-menu_container-links'>
+              <Menu />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
